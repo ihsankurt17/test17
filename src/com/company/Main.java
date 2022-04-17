c5b24b584fe14f3940b30dd99a9eda8d2026bc39
ihsankurt17 <ihsankurt@marun.edu.tr>
2022-04-17 02:31:09 +0200

    17 app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("kurt177");
         System.out.println("try kurt177");
+        System.out.println("try ihsan kurt177");
     }
 }
